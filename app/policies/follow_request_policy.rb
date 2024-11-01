429df1bd61b3537312cd2f71c2efa2308f517263
Vivian Davila <168599473+viviancreates@users.noreply.github.com>
2024-11-01 19:48:41 +0000

    Pause, site is down

---
+++ app/policies/follow_request_policy.rb
@@ -0,0 +1,12 @@
+class FollowRequestPolicy < ApplicationPolicy
+  attr_reader :user, :follow_request
+
+  def initialize(user, follow_request)
+    @user = user
+    @follow_request = follow_request
+  end
+
+  def show?
+    user == follow_request.sender || user == follow_request.recipient
+  end
+end
