c4ec67cf318042b7d956349f77f37cd6a9e7e9a5
Vivian Davila <168599473+viviancreates@users.noreply.github.com>
2024-11-01 18:24:13 +0000

    Auth method, do not need before action

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,8 +1,7 @@
 class PhotosController < ApplicationController
   before_action :set_photo, only: %i[ show edit update destroy ]
   before_action :ensure_current_user_is_owner, only: [:destroy, :update, :edit]
-  before_action :ensure_user_is_authorized, only: [:show]
-
+  
   # GET /photos or /photos.json
   def index
     @photos = Photo.all
@@ -70,12 +69,6 @@ class PhotosController < ApplicationController
   def ensure_current_user_is_owner
     if current_user != @photo.owner
       redirect_back fallback_location: root_url, alert: "You're not authorized for that."
-    end
-  end
-
-  def ensure_user_is_authorized
-    if !PhotoPolicy.new(current_user, @photo).show?
-      raise Pundit::NotAuthorizedError, "not allowed"
     end
   end
 
